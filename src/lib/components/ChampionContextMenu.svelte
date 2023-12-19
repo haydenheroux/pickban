0a67ac25c6b9f308b1c175929ec232303f55ee62
Hayden Heroux <haydenheroux@gmail.com>
2023-12-19 18:59:46 -0500

    refactor(drafting): Move context menu to its own component.

---
+++ src/lib/components/ChampionContextMenu.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+	import { getChampionNameByID } from "$lib/data/data_dragon";
+	import { Lane, addFavorite, hasFavorite, removeFavorite } from "$lib/data/stores";
+
+    export let championID: string | null = null;
+
+    export let x: number;
+    export let y: number;
+
+    let name: string;
+
+    $: {
+        name = getChampionNameByID(championID);
+    }
+
+    const lanes = [
+        {lane: Lane.Top, src: "top.png"},
+        {lane: Lane.Jungle, src: "jungle.png"},
+        {lane: Lane.Middle, src: "middle.png"},
+        {lane: Lane.Bottom, src: "bottom.png"},
+        {lane: Lane.Support, src: "support.png"},
+    ];
+</script>
+
+<nav class="context-menu" style="position: absolute; left: {x}px; top: {y}px;" on:contextmenu|preventDefault={() => {}}>
+    <ul>
+        <li><p>{name}</p></li>
+        <li><hr></li>
+        {#each lanes as {lane, src}}
+            {#if hasFavorite(lane, championID)}
+                <li>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <!-- svelte-ignore a11y-no-static-element-interactions -->
+                    <div on:click={() => removeFavorite(lane, championID)}>
+                        <!-- svelte-ignore a11y-missing-attribute -->
+                        <img {src}>
+                        <button>Unfavorite for {lane}</button>
+                    </div>
+                </li>
+            {:else}
+                <li>
+                    <!-- svelte-ignore a11y-click-events-have-key-events -->
+                    <!-- svelte-ignore a11y-no-static-element-interactions -->
+                    <div on:click={() => addFavorite(lane, championID)}>
+                        <!-- svelte-ignore a11y-missing-attribute -->
+                        <img {src}>
+                        <button>Favorite for {lane}</button>
+                    </div>
+                </li>
+            {/if}
+        {/each}
+    </ul>
+</nav>
+
+
+<style>
+    .context-menu {
+        display: inline-flex;
+        border: 1px solid var(--clr-border);
+        background-color: var(--clr-background);
+        border-radius: var(--radius-layout);
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .context-menu ul {
+        margin: 0.5rem;
+        padding: 0;
+    }
+
+    .context-menu ul li {
+        display: block;
+        list-style-type: none;
+        color: var(--clr-foreground);
+    }
+
+    .context-menu ul li div {
+        display: flex;
+        gap: 0.25rem;
+        align-items: center;
+    }
+
+    .context-menu ul li div img {
+        height: 1rem;
+        filter: grayscale();
+    }
+
+    .context-menu ul li div button {
+        font-size: 1rem;
+        width: 100%;
+        text-align: left;
+        border: 0px;
+        background-color: var(--clr-background);
+        color: var(--clr-muted);
+        white-space: nowrap;
+        padding: 0;
+    }
+
+    .context-menu ul li div:hover button {
+        color: var(--clr-foreground);
+    }
+
+    .context-menu ul li div:hover img {
+        color: var(--clr-foreground);
+        filter: grayscale() brightness(200%);
+    }
+</style>
--- src/lib/components/ChampionFrame.svelte
+++ src/lib/components/ChampionFrame.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { getChampionNameByID, getImageURL } from "$lib/data/data_dragon";
-	import { Lane, addFavorite, hideContextMenus, hasFavorite, removeFavorite } from "$lib/data/stores";
+	import { hideContextMenus } from "$lib/data/stores";
 	import { createEventDispatcher } from "svelte";
+	import ChampionContextMenu from "./ChampionContextMenu.svelte";
 
     export let big: boolean = false;
     export let hideName: boolean = false;
@@ -36,17 +37,7 @@
         imageURL = getImageURL(championID);
     }
 
-    function getContextMenuDimension(node: HTMLElement) {
-        menuDimension = {
-            h: node.offsetHeight,
-            w: node.offsetWidth
-        }
-    }
-
-    let mouse = {x: 0, y: 0};
     let menu = {x: 0, y: 0};
-    let menuDimension = {h: 0, w: 0};
-    let browser = {x: 0, y: 0, w: 0, h: 0};
 
     let showContextMenu: boolean = false;
 
@@ -59,32 +50,13 @@
 
         showContextMenu = true;
 
-        mouse = {
-            x: e.pageX,
-            y: e.pageY
-        }
-        browser = {
-            x: window.scrollX,
-            y: window.scrollY,
-            w: window.innerWidth,
-            h: window.innerHeight
-        }
-
-        menu.x = mouse.x;
-        menu.y = mouse.y;
+        menu.x = e.pageX;
+        menu.y = e.pageY;
     }
 
     function closeContextMenu() {
         showContextMenu = false;
     }
-
-    const lanes = [
-        {lane: Lane.Top, src: "top.png"},
-        {lane: Lane.Jungle, src: "jungle.png"},
-        {lane: Lane.Middle, src: "middle.png"},
-        {lane: Lane.Bottom, src: "bottom.png"},
-        {lane: Lane.Support, src: "support.png"},
-    ];
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
@@ -103,35 +75,7 @@
 </div>
 
 {#if showContextMenu}
-    <nav use:getContextMenuDimension class="context-menu" style="position: absolute; left: {menu.x}px; top: {menu.y}px;" on:contextmenu|preventDefault={() => {}}>
-        <ul>
-            <li><p>{name}</p></li>
-            <li><hr></li>
-            {#each lanes as {lane, src}}
-                {#if hasFavorite(lane, championID)}
-                    <li>
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <!-- svelte-ignore a11y-no-static-element-interactions -->
-                        <div on:click={() => removeFavorite(lane, championID)}>
-                            <!-- svelte-ignore a11y-missing-attribute -->
-                            <img {src}>
-                            <button>Unfavorite for {lane}</button>
-                        </div>
-                    </li>
-                {:else}
-                    <li>
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <!-- svelte-ignore a11y-no-static-element-interactions -->
-                        <div on:click={() => addFavorite(lane, championID)}>
-                            <!-- svelte-ignore a11y-missing-attribute -->
-                            <img {src}>
-                            <button>Favorite for {lane}</button>
-                        </div>
-                    </li>
-                {/if}
-            {/each}
-        </ul>
-    </nav>
+    <ChampionContextMenu {championID} x={menu.x} y={menu.y}/>
 {/if}
 
 <svelte:window on:click={closeContextMenu} on:message={() => showContextMenu = false}/>
@@ -189,57 +133,5 @@
 
     .name.disabled {
         color: var(--clr-border);
-    }
-
-    .context-menu {
-        display: inline-flex;
-        border: 1px solid var(--clr-border);
-        background-color: var(--clr-background);
-        border-radius: var(--radius-layout);
-        overflow: hidden;
-        display: flex;
-        flex-direction: column;
-    }
-
-    .context-menu ul {
-        margin: 0.5rem;
-        padding: 0;
-    }
-
-    .context-menu ul li {
-        display: block;
-        list-style-type: none;
-        color: var(--clr-foreground);
-    }
-
-    .context-menu ul li div {
-        display: flex;
-        gap: 0.25rem;
-        align-items: center;
-    }
-
-    .context-menu ul li div img {
-        height: 1rem;
-        filter: grayscale();
-    }
-
-    .context-menu ul li div button {
-        font-size: 1rem;
-        width: 100%;
-        text-align: left;
-        border: 0px;
-        background-color: var(--clr-background);
-        color: var(--clr-muted);
-        white-space: nowrap;
-        padding: 0;
-    }
-
-    .context-menu ul li div:hover button {
-        color: var(--clr-foreground);
-    }
-
-    .context-menu ul li div:hover img {
-        color: var(--clr-foreground);
-        filter: grayscale() brightness(200%);
     }
 </style>
