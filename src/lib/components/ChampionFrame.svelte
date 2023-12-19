12f45ee4ee925cf17ae2a45186c445d8f0c8220e
Hayden Heroux <haydenheroux@gmail.com>
2023-12-19 02:12:41 -0500

    WIP.

--- src/lib/components/ChampionFrame.svelte
+++ src/lib/components/ChampionFrame.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
 	import { getChampionNameByID, getImageURL } from "$lib/data/data_dragon";
 	import { Lane, addFavorite, hideContextMenus, hasFavorite, removeFavorite } from "$lib/data/stores";
+	import { createEventDispatcher } from "svelte";
+
+    export let hideName: boolean = false;
+
+    export let selected: boolean = false;
+
+    const dispatch = createEventDispatcher();
+
+    function onClick() {
+        dispatch("message", {
+            championID: championID,
+            setChampionID: (newChampionID: string | null) => {
+                championID = newChampionID;
+            }
+        });
+    }
+
+    export let disabled: boolean = false;
 
     export let championID: string | null = null;
 
-    let name: string = getChampionNameByID(championID);
-    let imageURL: string = getImageURL(championID);
+    let name: string;
+    let imageURL: string;
+
+    $: {
+        name = getChampionNameByID(championID);
+        imageURL = getImageURL(championID);
+    }
 
     function getContextMenuDimension(node: HTMLElement) {
         menuDimension = {
@@ -51,14 +74,15 @@
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<div class="champion-frame" on:contextmenu|preventDefault={openContextMenu}>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="champion-frame" on:contextmenu|preventDefault={openContextMenu} on:click|preventDefault={onClick}>
     {#if imageURL.length > 0 }
-        <div class="portrait" style="background-image: url({imageURL});" />
+        <div class="portrait {selected ? "gold" : ""} {disabled ? "disabled" : ""}" style="background-image: url({imageURL});" />
     {:else}
         <div class="portrait" style="border: 1px solid var(--clr-neutral-800);" />
     {/if}
-    {#if name.length > 0}
-        <p class="name">
+    {#if name.length > 0 && !hideName}
+        <p class="name {selected ? "gold": ""} {disabled ? "disabled" : ""}">
             {name}
         </p>
     {/if}
@@ -102,12 +126,31 @@
         background-position: center;
         background-repeat: no-repeat;
 
+        border-width: 1px;
+        border-style: solid;
+        border-color: transparent;
         border-radius: var(--radius-element);
+    }
+
+    .portrait.gold {
+        border-color: var(--clr-gold);
+    }
+
+    .portrait.disabled {
+        filter: grayscale(100%);
     }
 
     .name {
         font-size: var(--fs-100);
         white-space: nowrap;
+    }
+
+    .name.gold {
+        color: var(--clr-gold);
+    }
+
+    .name.disabled {
+        color: var(--clr-border);
     }
 
     .context-menu {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,61 @@
-<script>
+<script lang="ts">
 	import ChampionFrame from "$lib/components/ChampionFrame.svelte";
 	import { championIDs } from "$lib/data/data_dragon";
+
+	let selected: string | null = null;
+
+	let blueBans: Array<string | null> = [null, null, null, null, null];
+	let redBans = [null, null, null, null, null];
+
+	let bluePicks = [null, null, null, null, null];
+	let redPicks = [null, null, null, null, null];
+
+	function handle(event: any) {
+		const championID = event.detail.championID;
+
+		const selectionIsEmpty = selected == null;
+		const selectionIsNotEmpty = !selectionIsEmpty;
+		const clickedIsEmpty = championID == null;
+		const clickedIsNotEmpty = !clickedIsEmpty;
+
+		if (selectionIsEmpty) {
+			selected = championID;
+		} else if (selectionIsNotEmpty && clickedIsEmpty) {
+			event.detail.setChampionID(selected);
+			selected = null;
+		} else if (selectionIsNotEmpty && clickedIsNotEmpty) {
+			// Try to swap
+			event.detail.setChampionID(selected);
+			selected = null;
+		}
+
+		generateSelectedMap();
+	}
+
+	let selectedMap: Record<string, boolean> = {};
+	generateSelectedMap();
+
+	function generateSelectedMap() {
+		for (let championID of championIDs) {
+			selectedMap[championID] = championID == selected;
+		}
+	}
 </script>
 
 <div class="bans-container">
 	<div class="bans">
-		<ChampionFrame />
-		<ChampionFrame />
-		<ChampionFrame />
-		<ChampionFrame />
-		<ChampionFrame />
+		<ChampionFrame bind:championID={blueBans[0]} on:message={handle} hideName={true} />
+		<ChampionFrame bind:championID={blueBans[1]} on:message={handle} hideName={true} />
+		<ChampionFrame bind:championID={blueBans[2]} hideName={true} />
+		<ChampionFrame bind:championID={blueBans[3]} hideName={true} />
+		<ChampionFrame bind:championID={blueBans[4]} hideName={true} />
 	</div>
 	<div class="bans">
-		<ChampionFrame />
-		<ChampionFrame />
-		<ChampionFrame />
-		<ChampionFrame />
-		<ChampionFrame />
+		<ChampionFrame bind:championID={redBans[0]} hideName={true} />
+		<ChampionFrame bind:championID={redBans[1]} hideName={true} />
+		<ChampionFrame bind:championID={redBans[2]} hideName={true} />
+		<ChampionFrame bind:championID={redBans[3]} hideName={true} />
+		<ChampionFrame bind:championID={redBans[4]} hideName={true} />
 	</div>
 </div>
 
@@ -24,49 +63,49 @@
 	<div class="picks">
 		<div>
 			<h2 class="blue">B1</h2>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={bluePicks[0]} hideName={true} />
 		</div>
 		<div>
 			<h2 class="blue">B2</h2>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={bluePicks[1]} hideName={true} />
 		</div>
 		<div>
 			<h2 class="blue">B3</h2>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={bluePicks[2]} hideName={true} />
 		</div>
 		<div>
 			<h2 class="blue">B4</h2>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={bluePicks[3]} hideName={true} />
 		</div>
 		<div>
 			<h2 class="blue">B5</h2>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={bluePicks[4]} hideName={true} />
 		</div>
 	</div>
 	<div class="picker">
 		{#each championIDs as championID}
-			<ChampionFrame {championID}/>
+			<ChampionFrame {championID} on:message={handle} bind:selected={selectedMap[championID]}/>
 		{/each}
 	</div>
 	<div class="picks">
 		<div>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={redPicks[0]} hideName={true} />
 			<h2 class="red">R1</h2>
 		</div>
 		<div>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={redPicks[1]} hideName={true} />
 			<h2 class="red">R2</h2>
 		</div>
 		<div>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={redPicks[2]} hideName={true} />
 			<h2 class="red">R3</h2>
 		</div>
 		<div>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={redPicks[3]} hideName={true} />
 			<h2 class="red">R4</h2>
 		</div>
 		<div>
-			<ChampionFrame />
+			<ChampionFrame bind:championID={redPicks[4]} hideName={true} />
 			<h2 class="red">R5</h2>
 		</div>
 	</div>
