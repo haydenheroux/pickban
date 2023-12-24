ef682b5683529450bc3403bd6c7877479a01efe7
Hayden Heroux <haydenheroux@gmail.com>
2023-12-24 04:29:57 -0500

    refactor(map): Rename canvas to canvasOrNull.

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -3,31 +3,32 @@
 
     let imageOrNull: HTMLImageElement | null = null;
 
-    let canvas: HTMLCanvasElement;
+    let canvasOrNull: HTMLCanvasElement | null;
 
     onMount(() => {
             imageOrNull = new Image();
             imageOrNull.src = "map.jpg";
             imageOrNull.onload = () => {
+                if (!canvasOrNull) return;
                 if (!imageOrNull) return;
 
-                canvas.width = canvas.offsetWidth;
-                canvas.height = canvas.offsetWidth * (imageOrNull.height / imageOrNull.width);
+                canvasOrNull.width = canvasOrNull.offsetWidth;
+                canvasOrNull.height = canvasOrNull.offsetWidth * (imageOrNull.height / imageOrNull.width);
                 draw();
             };
         });
 
     function draw() {
-		if (!canvas) return;
+		if (!canvasOrNull) return;
 		if (!imageOrNull) return;
 
-		const ctx = canvas.getContext('2d');
-		if (!ctx) return;
+		const ctxOrNull = canvasOrNull.getContext('2d');
+		if (!ctxOrNull) return;
 		
-        ctx.drawImage(imageOrNull, 0, 0, canvas.width, canvas.height);
+        ctxOrNull.drawImage(imageOrNull, 0, 0, canvasOrNull.width, canvasOrNull.height);
     }
 </script>
 
 <section>
-    <canvas bind:this={canvas} />
+    <canvas bind:this={canvasOrNull} />
 </section>
