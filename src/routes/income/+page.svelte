a65099cb959e8d0d6e20c5ad7809aaa48fc544b0
Hayden Heroux <haydenheroux@gmail.com>
2023-12-28 01:55:38 -0500

    feat(income): Implement calculation for jungle.

--- src/routes/income/+page.svelte
+++ src/routes/income/+page.svelte
@@ -27,12 +27,13 @@
         campsSlainPerMinute = calculateCreepScorePerMinute(creepScore / 4, totalMinutes);
 
         let minions = Minions.until(totalMinutes);
+        let camps = Camps.until(totalMinutes);
 
         minionEfficiency = coerce(creepScore / minions.getCreepScore());
-        campEfficiency = 0;
+        campEfficiency = coerce(creepScore / camps.getCreepScore());
 
         minionGoldEarned = minionEfficiency * minions.getGoldIncome();
-        campGoldEarned = 0;
+        campGoldEarned = campEfficiency * camps.getGoldIncome();
     }
 
     function coerce(n: number) {
@@ -50,6 +51,15 @@
         private static minutesPerWave: number = (30 / 60);
         private static wavesPerMinute: number = 1 / this.minutesPerWave;
 
+        private static wavesUntil(minutes: number) {
+            // The "+ 1" includes the initial wave that spawns at initialWaveSpawnDelayMinutes
+            return Math.floor(1 + (minutes - this.initialWaveSpawnDelayMinutes) * this.wavesPerMinute);
+        }
+
+        private static minutesAt(wave: number) {
+            return this.initialWaveSpawnDelayMinutes + (wave - 1) * this.minutesPerWave;
+        }
+
         private melees!: number;
         private static goldPerMelee: number = 21;
 
@@ -65,16 +75,15 @@
         private stage3Cannons!: number;
         private static goldPerStage3Cannon: number = 90;
 
-        private static wavesUntil(minutes: number) {
-            // The "+ 1" includes the initial wave that spawns at initialWaveSpawnDelayMinutes
-            return Math.floor(1 + (minutes - this.initialWaveSpawnDelayMinutes) * this.wavesPerMinute);
+        private constructor() {
+            this.melees = 0;
+            this.ranged = 0;
+            this.stage1Cannons = 0;
+            this.stage2Cannons = 0;
+            this.stage3Cannons = 0;
         }
 
-        private static minutesAt(wave: number) {
-            return this.initialWaveSpawnDelayMinutes + (wave - 1) * this.minutesPerWave;
-        }
-
-        public static until(minutes: number) {
+        public static until(minutes: number): Minions {
             const minions: Minions = new Minions(); 
 
             const waves = this.wavesUntil(minutes);
@@ -97,14 +106,6 @@
             return minions;
         }
 
-        private constructor() {
-            this.melees = 0;
-            this.ranged = 0;
-            this.stage1Cannons = 0;
-            this.stage2Cannons = 0;
-            this.stage3Cannons = 0;
-        }
-
         public getCreepScore(): number {
             return this.melees + this.ranged + this.stage1Cannons + this.stage2Cannons + this.stage3Cannons;
         }
@@ -115,6 +116,88 @@
                 + this.stage1Cannons * Minions.goldPerStage1Cannon 
                 + this.stage2Cannons * Minions.goldPerStage2Cannon 
                 + this.stage3Cannons * Minions.goldPerStage3Cannon;
+        }
+    }
+
+    class Camps {
+        private redBuffs!: number;
+        private static redBuffInitialSpawnMinutes: number = 1 + (30 / 60);
+        private static redBuffRespawnDelayMinutes: number = 5;
+        private static redBuffCreepScore: number = 4;
+        private static redBuffGold: number = 90;
+
+        private krugs!: number;
+        private static krugsInitialSpawnMinutes: number = 1 + (42 / 60);
+        private static krugsRespawnDelayMinutes: number = 2 + (15 / 60);
+        private static krugsCreepScore: number = 4;
+        private static krugsGold: number = 15 + 10 + 6 * 14;
+
+        private raptors!: number;
+        private static raptorsInitialSpawnMinutes: number = 1 + (30 / 60);
+        private static raptorsRespawnDelayMinutes: number = 2 + (15 / 60);
+        private static raptorsCreepScore: number = 4;
+        private static raptorsGold: number = 35 + 5 * 8;
+
+        private wolves!: number;
+        private static wolvesInitialSpawnMinutes: number = 1 + (30 / 60);
+        private static wolvesRespawnDelayMinutes: number = 2 + (15 / 60);
+        private static wolvesCreepScore: number = 4;
+        private static wolvesGold: number = 55 + 2 * 13;
+
+        private blueBuffs!: number;
+        private static blueBuffInitialSpawnMinutes: number = 1 + (30 / 60);
+        private static blueBuffRespawnDelayMinutes: number = 5;
+        private static blueBuffCreepScore: number = 4;
+        private static blueBuffGold: number = 90;
+
+        private gromps!: number;
+        private static grompInitialSpawnMinutes: number = 1 + (42 / 60);
+        private static grompRespawnDelayMinutes: number = 2 + (15 / 60);
+        private static grompCreepScore: number = 4;
+        private static grompGold: number = 80;
+
+        private constructor() {
+            this.redBuffs = 0;
+            this.krugs = 0;
+            this.raptors = 0;
+            this.wolves = 0;
+            this.blueBuffs = 0;
+            this.gromps = 0;
+        }
+
+        private static spawns(minutes: number, initialSpawnMinutes: number, respawnDelayMinutes: number): number {
+            return 1 + (minutes - initialSpawnMinutes) / respawnDelayMinutes;
+        }
+
+        public static until(minutes: number): Camps {
+            const camps: Camps = new Camps();
+
+            camps.redBuffs = Camps.spawns(minutes, this.redBuffInitialSpawnMinutes, this.redBuffRespawnDelayMinutes);
+            camps.krugs = Camps.spawns(minutes, this.krugsInitialSpawnMinutes, this.krugsRespawnDelayMinutes);
+            camps.raptors = Camps.spawns(minutes, this.raptorsInitialSpawnMinutes, this.raptorsRespawnDelayMinutes);
+            camps.wolves = Camps.spawns(minutes, this.wolvesInitialSpawnMinutes, this.wolvesRespawnDelayMinutes);
+            camps.blueBuffs = Camps.spawns(minutes, this.blueBuffInitialSpawnMinutes, this.blueBuffRespawnDelayMinutes);
+            camps.gromps = Camps.spawns(minutes, this.grompInitialSpawnMinutes, this.grompRespawnDelayMinutes);
+
+            return camps;
+        }
+
+        public getCreepScore() {
+            return this.redBuffs * Camps.redBuffCreepScore
+                + this.krugs * Camps.krugsCreepScore
+                + this.raptors * Camps.raptorsCreepScore
+                + this.wolves * Camps.wolvesCreepScore
+                + this.blueBuffs * Camps.blueBuffCreepScore
+                + this.gromps * Camps.grompCreepScore;
+        }
+
+        public getGoldIncome() {
+            return this.redBuffs * Camps.redBuffGold
+                + this.krugs * Camps.krugsGold
+                + this.raptors * Camps.raptorsGold
+                + this.wolves * Camps.wolvesGold
+                + this.blueBuffs * Camps.blueBuffGold
+                + this.gromps * Camps.grompGold;
         }
     }
 </script>
