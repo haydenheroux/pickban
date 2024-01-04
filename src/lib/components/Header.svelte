56e7b40ae6b611714bd2ff088e8aa7f8962a0a86
Hayden Heroux <haydenheroux@gmail.com>
2024-01-04 14:50:22 -0500

    feat(damage): WIP damage calculator.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,6 +9,7 @@
 			<a href="{base}/"><em>drafting</em></a>
 			<HeaderLink href="{base}/map">map</HeaderLink>
 			<HeaderLink href="{base}/income">income</HeaderLink>
+			<HeaderLink href="{base}/damage">damage</HeaderLink>
 		</nav>
 	</div>
 </header>
