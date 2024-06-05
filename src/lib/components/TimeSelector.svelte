609990ad4569d11db2a35ba4b2d8a85efe7cb50c
Hayden Heroux <haydenheroux@gmail.com>
2024-06-05 14:00:02 -0400

    fix: Improve time formatting.

--- src/lib/components/TimeSelector.svelte
+++ src/lib/components/TimeSelector.svelte
@@ -1,12 +1,45 @@
 <script lang="ts">
     export let minutes: number = 0;
     export let seconds: number = 0;
+
+    let minutesValue: string = "0";
+    let secondsValue: string = "00";
+
+    $: {
+        minutes = Number(minutesValue);
+    }
+
+    $: {
+        seconds = Number(secondsValue);
+    }
+
+    function format(event: Event) {
+        const target = event.target;
+
+        if (!target) return;
+
+        const input = target as HTMLInputElement;
+
+        const value = input.value;
+
+        let result;
+
+        if (value.length == 1) {
+            result = "0" + value;
+        } else if (value == "0") {
+            result = "00";
+        } else {
+            result = value;
+        }
+
+        input.value = result;
+    }
 </script>
 
 <div class="time">
-    <input bind:value={minutes} type="number" min="0" max="59">
+    <input bind:value={minutesValue} type="number" min="0" max="59">
     <span>:</span>
-    <input bind:value={seconds} type="number" min="0" max="59">
+    <input bind:value={secondsValue} type="number" min="0" max="59" on:input={format}>
 </div>
 
 <style>
